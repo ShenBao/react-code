f73b8a627183b10ba50060826778c06bb957983a
shenbao <shenbaoone@gmail.com>
2017-07-20 21:56:42 +0800

    add my-app... README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 
 
-var pro = 'hello-word';
+var pro = 'test';
 var dist = './'+pro;
 
 var gulp = require('gulp'),
     clean = require('gulp-clean');
 
 gulp.task('copy', function () {
-    gulp.src(['./my-app-demo/package.json','./my-app-demo/yarn.lock'])
+    gulp.src(['./_my-app-demo/package.json','./_my-app-demo/yarn.lock'])
         .pipe(gulp.dest(dist));
 });
 gulp.task('copySrc', function () {
-    gulp.src(['./my-app-demo/src/**/*'])
+    gulp.src(['./_my-app-demo/src/**/*'])
         .pipe(gulp.dest(dist+'/src'));
 });
 
 gulp.task('copyPublic', function () {
-    gulp.src(['./my-app-demo/public/**/*'])
+    gulp.src(['./_my-app-demo/public/**/*'])
         .pipe(gulp.dest(dist+'/public'));
 });
 
 gulp.task('copyBuild', function () {
-    gulp.src(['./my-app-demo/build/**/*'])
+    gulp.src(['./_my-app-demo/build/**/*'])
         .pipe(gulp.dest(dist+'/build'));
 });
 
